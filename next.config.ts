961943e055fa986672ef17586bc182dcae7b5aa0
ahayder <alihayder19@gmail.com>
2026-03-28 03:01:12 +0600

    feat: Make Next.js `assetPrefix` port configurable via the `PORT` environment variable.

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,14 @@ import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === "production";
 const internalHost = process.env.TAURI_DEV_HOST || "localhost";
+const internalPort = process.env.PORT || "5005";
 
 const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  assetPrefix: isProd ? undefined : `http://${internalHost}:5005`,
+  assetPrefix: isProd ? undefined : `http://${internalHost}:${internalPort}`,
 };
 
 export default nextConfig;
